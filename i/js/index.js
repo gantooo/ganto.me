36252fb9360ebb42d58b40260fd20f87d9caaefe
Ganto <77222539+gantooo@users.noreply.github.com>
2021-09-10 12:18:54 +0800

    Update index.js

--- i/js/index.js
+++ i/js/index.js
@@ -7,7 +7,7 @@ $(function(){
 		$(".cons .right_").hide();
 		$(".cons .left_").show();
 		$(".con").show();
-		$(".cons").css({"right":"30%","box-shadow":"4px 0px 4px rgba(0,0,0,0.5)"});
+		$(".cons").css({"left":"0","box-shadow":"4px 0px 4px rgba(0,0,0,0.5)"});
 	})
 	$(".con,.cons .left_").click(function(){
 		$("body").css({"overflow":"auto"});
@@ -15,7 +15,7 @@ $(function(){
 		$(".cons .left_").hide();
 		$(".cons .right_").show();
 		$(".con").hide();
-		$(".cons").css({"right":"100%","box-shadow":"4px 0px 4px rgba(0,0,0,0)"});
+		$(".cons").css({"left":"-100%","box-shadow":"4px 0px 4px rgba(0,0,0,0)"});
 	})
 	console.log("%cGanto | ganto.cn","background-color:#3742FA;color:white;border-radius:4px;")
 })
