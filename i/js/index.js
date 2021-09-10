51607a02bddfb616eb23df2a2972acd7e5e57cbb
Ganto <77222539+gantooo@users.noreply.github.com>
2021-09-10 12:22:38 +0800

    Update index.js

--- i/js/index.js
+++ i/js/index.js
@@ -9,13 +9,16 @@ $(function(){
 		$(".con").show();
 		$(".cons").css({"left":"0","box-shadow":"4px 0px 4px rgba(0,0,0,0.5)"});
 	})
+	let CW = $(".cons").width();
+	let CWPX = CW + "px"
+	console.log(CW, CWPX);
 	$(".con,.cons .left_").click(function(){
 		$("body").css({"overflow":"auto"});
 		
 		$(".cons .left_").hide();
 		$(".cons .right_").show();
 		$(".con").hide();
-		$(".cons").css({"left":"-100%","box-shadow":"4px 0px 4px rgba(0,0,0,0)"});
+		$(".cons").css({"left":CWPX,"box-shadow":"4px 0px 4px rgba(0,0,0,0)"});
 	})
 	console.log("%cGanto | ganto.cn","background-color:#3742FA;color:white;border-radius:4px;")
 })
