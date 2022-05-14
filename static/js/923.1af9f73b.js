5fcc7e937c06a88b17ac9164fff736ddd0a878c0
ganto-cn <i@ganto.icu>
2022-05-15 00:22:19 +0800

    update notice click display

--- static/js/923.1af9f73b.js
+++ static/js/923.1af9f73b.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunkganto"]=self["webpackChunkganto"]||[]).push([[923],{1923:function(n,t,a){a.r(t),a.d(t,{default:function(){return l}});var e=a(3396),r=a(7139);const u={class:"article",id:"home"};function i(n,t,a,i,s,c){return(0,e.wg)(),(0,e.iD)("div",u,[((0,e.wg)(!0),(0,e.iD)(e.HY,null,(0,e.Ko)(s.pList,(n=>((0,e.wg)(),(0,e.iD)("p",null,(0,r.zw)(n),1)))),256))])}var s={name:"Home",data(){return{pList:["愿中国青年都摆脱冷气，只是向上走，不必听自暴自弃者流的话。能做事的做事，能发声的发声。有一分热，发一分光，就令萤火一般，也可以在黑暗里发一点光，不必等候炬火。","此后如竟没有炬火：我便是唯一的光。倘若有了炬火，出了太阳，我们自然心悦诚服的消失。不但毫无不平，而且还要随喜赞美这炬火或太阳；因为他照了人类，连我都在内。","鲁迅先生《热风·随感录四十一》"]}}},c=a(89);const o=(0,c.Z)(s,[["render",i],["__scopeId","data-v-7701a76a"]]);var l=o}}]);
-//# sourceMappingURL=923.4bb7f015.js.map
+//# sourceMappingURL=923.1af9f73b.js.map
